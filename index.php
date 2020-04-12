806c27dfd22db13dc839da7288fc37e1f06abef2
kingofuzbek <63531467+kingofuzbek@users.noreply.github.com>
2020-04-12 12:12:00 +0500

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  *
  *Telegram : @TILON
  */
-$API_KEY = 'token';
+$API_KEY = '1151929874:AAETtkMsFipGfcM8OEnHxrtI22sderWXqPk';
 ##------------------------------##
 define('API_KEY',$API_KEY);
 function bot($method,$datas=[]){
